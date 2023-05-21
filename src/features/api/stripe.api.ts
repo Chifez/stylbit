aa66c14f2bee68e457778f87fce82e414815c9b1
emmanuel <chifez1@gmail.com>
2023-05-21 14:44:45 +0100

    feat:trying again

--- src/features/api/stripe.api.ts
+++ src/features/api/stripe.api.ts
@@ -2,7 +2,7 @@ import { loadStripe, Stripe } from "@stripe/stripe-js";
 import axios from "axios";
 
 const keyApi = axios.create({
-  baseURL: "https://stylbit-sever-git-master-chifez.vercel.app:400/",
+  baseURL: "https://stylbit-sever-git-master-chifez.vercel.app/",
 });
 
 export const getPublicKey = async (setStripePromise: (e: any) => void) => {
