8904d27f9c3ad6a2d6508ee7fbbd2f0c7a6b3e58
emmanuel <chifez1@gmail.com>
2023-05-21 14:30:56 +0100

    fix:changed the base url

--- src/features/api/stripe.api.ts
+++ src/features/api/stripe.api.ts
@@ -2,18 +2,18 @@ import { loadStripe, Stripe } from "@stripe/stripe-js";
 import axios from "axios";
 
 const keyApi = axios.create({
-	baseURL: "http://localhost:400/",
+  baseURL: "https://stylbit-sever-git-master-chifez.vercel.app/",
 });
 
 export const getPublicKey = async (setStripePromise: (e: any) => void) => {
-	const publicKey = await keyApi.get("/config");
-	const { publishablekey } = publicKey.data;
-	return setStripePromise(await loadStripe(publishablekey));
+  const publicKey = await keyApi.get("/config");
+  const { publishablekey } = publicKey.data;
+  return setStripePromise(await loadStripe(publishablekey));
 };
 
 export const getSecretKey = async (setClientSecret: (e: any) => void) => {
-	await keyApi.post("/payment", {}).then((res) => {
-		const { clientSecret } = res.data;
-		return setClientSecret(clientSecret);
-	});
+  await keyApi.post("/payment", {}).then((res) => {
+    const { clientSecret } = res.data;
+    return setClientSecret(clientSecret);
+  });
 };
